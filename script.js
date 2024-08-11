42dd9969f4a4a08c7ee28052ef5c32f33a784c4f
WavyCoder <53162178+DatravousOdds@users.noreply.github.com>
2024-08-11 01:37:01 +0000

    error check logic dn

--- script.js
+++ script.js
@@ -5,28 +5,40 @@ let numberOfYears = 0;
 let compoundedInterest = 1;
 let endOfInvestment = 0.00;
 
-// prompt for input
-principalAmount = prompt("What is the principal amount?");
-annualInterestRate = prompt("What is the rate");
-numberOfYears = prompt("What is the number of years");
+
+while (true) {
+  // prompt for input
+  principalAmount = prompt("What is the principal amount? ");
+  annualInterestRate = prompt("What is the rate? ");
+  numberOfYears = prompt("What is the number of years? ");
+  compoundedInterest = prompt("What is the number of times interest is compounded per year? ");
+
+  // check for invalid input
+  if (isNaN(principalAmount) || principalAmount < 0 || isNaN(annualInterestRate) || annualInterestRate < 0 || isNaN(numberOfYears) || numberOfYears < 0) {
+    alert("Please enter valid input for all fields");
+    continue;
+  } else {
+    // convert input to numerical date
+    principalAmount = parseFloat(principalAmount);
+    annualInterestRate = parseFloat(annualInterestRate);
+    numberOfYears = parseInt(numberOfYears);
+    compoundedInterest = parseInt(compoundedInterest);
+
+    // convert annual interest rate to monthly interest rate
+    annualInterestRate = (annualInterestRate / 100);
+
+
+    // calculate compound interest
+    endOfInvestment = principalAmount * Math.pow((1 + annualInterestRate / compoundedInterest), compoundedInterest * numberOfYears);
 
 
 
-// convert input to numerical date
-principalAmount = parseFloat(principalAmount);
-annualInterestRate = parseFloat(annualInterestRate);
-numberOfYears = parseInt(numberOfYears);
-compoundedInterest = parseInt(compoundedInterest);
+    // Display results
+    alert(`$${principalAmount} invested at ${annualInterestRate * 100}% for ${numberOfYears} years compounded ${compoundedInterest} times per year is $${endOfInvestment.toFixed(2)}.`);
+    break;
+  }
+}
 
-annualInterestRate = (annualInterestRate / 100);
-console.log(annualInterestRate);
 
-// calculate compound interest
-endOfInvestment = principalAmount * Math.pow((1 + annualInterestRate / compoundedInterest), compoundedInterest * numberOfYears);
-console.log("End of Investment: ", endOfInvestment.toFixed(2));
 
-// Round to 2 decimal places
-// Math.round(endOfInvestment)
 
-// Display results
-// consloe.log("The end of investment is " + endOfInvestment)
